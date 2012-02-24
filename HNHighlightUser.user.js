b24feecd15b25d6fa1d305f50718eb064f942a45
SamHasler <flenser.tyrathect+github@gmail.com>
2012-02-24 15:31:01 +0000

    Prepend identicon instead of highlight link. Made highlights orange to stand out more.

--- HNHighlightUser.user.js
+++ HNHighlightUser.user.js
@@ -1,47 +1,66 @@
 // ==UserScript==
 // @name           HNHighlightUser
-// @description    This script adds a "highlight" link next to posts on HN, so you can easily highlight authors in a single thread
+// @description    This script adds identicons next to posts on HN, so you can easily follow authors.
 // @include        http://news.ycombinator.com/*
 // @include        https://news.ycombinator.com/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js
+// @require        https://raw.github.com/SamHasler/identicon/master/identicon.js
 // ==/UserScript==
 
+var IDENTICON_SIZE = 11;
+
+//From http://stackoverflow.com/a/8076436/2541
+function hashCode(string){
+    var hash = 0;
+    if (string.length == 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+    
+var identiconsCache = {};
+
+function getDataUrlForAuthor(author){
+    if (identiconsCache[author] != null){
+        return identiconsCache[author];
+    }
+    new Identicon('identiconCanvas', hashCode(author), IDENTICON_SIZE);
+    var canvas = document.getElementById('identiconCanvas');
+    var dataURL = canvas.toDataURL("image/png");
+    identiconsCache[author] = dataURL;
+    return dataURL;
+}
 
 //the style that is applied to highlighted posts.
-var highlightClass = "<style>.highlight {background-color:#EEEEEE;}</style>";
-
+var highlightClass = "<style>.highlight {background-color:#FFB380;}</style>";
 
 if (window.location.pathname === "/item") {
 
-	$("body").prepend(highlightClass);
+    $("body").prepend('<canvas id="identiconCanvas" style="display:none;" width="'+ IDENTICON_SIZE +'" height="'+ IDENTICON_SIZE +'"></canvas>');
 	
-	$("span.comhead").each(function() {
-		var commentDetails = $(this).text().match(/([A-Za-z0-9_]+) ([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
-		
-		//The comhead acts slightly differently depending on whether its "Ask HN" type post or a link
-		//Links do not have comhead details so we have to check for null
-		if(commentDetails !== null) {
-			var commentAuthor = commentDetails[1];
-			
+    $("span.comhead").each(function() {
+        var commentDetails = $(this).text().match(/([A-Za-z0-9_]+) ([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
+        
+        //The comhead acts slightly differently depending on whether its "Ask HN" type post or a link
+        //Links do not have comhead details so we have to check for null
+        if(commentDetails !== null) {
+            var commentAuthor = commentDetails[1];
+            
 			//very simple setup here.  Each highlight link's id is the author
-			$(this).append(" | <a href='#' class='highlightLink' id='" + commentAuthor + "'>highlight</a>");
-			
+            $(this).prepend("<img class='highlightLink' id='" + commentAuthor + "' src='" + getDataUrlForAuthor(commentAuthor) + "'> ");
+
 			//and each comment table row gets a class name that corresponds to the author
 			$(this).parents().eq(1).addClass(commentAuthor);
-		}
-	});
-
+			
+        }
+    });
     
-	
 	//when we want to highlight an author, just find all the classes that match the author name and toggle the class
 	$(".highlightLink").click(function(e){
 		$("." + this.id).toggleClass("highlight");
 		e.preventDefault();
-    });
-
-
+	});
 }
-
-
-
-
